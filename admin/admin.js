d9db004f870d5dc6600c577d943f7b7d2af6037a
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-15 18:31:49 +0700

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -101,3 +101,116 @@ alert(json.message);
 
 
 }
+const API="/api/products";
+
+
+
+async function addProduct(){
+
+
+let data={
+
+name:name.value,
+
+category:category.value,
+
+price:Number(price.value)
+
+};
+
+
+
+await fetch(API,{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":
+"application/json"
+
+},
+
+body:
+JSON.stringify(data)
+
+});
+
+
+alert(
+"Produk ditambahkan"
+);
+
+
+loadProduct();
+
+
+}
+
+
+
+
+async function loadProduct(){
+
+
+let res =
+await fetch(API);
+
+
+let data =
+await res.json();
+
+
+
+list.innerHTML="";
+
+
+
+data.forEach(p=>{
+
+
+list.innerHTML += `
+
+<tr>
+
+<td>
+${p.name}
+</td>
+
+<td>
+${p.category}
+</td>
+
+<td>
+Rp${p.price}
+</td>
+
+
+<td>
+
+<button>
+Edit
+</button>
+
+
+<button>
+Hapus
+</button>
+
+
+</td>
+
+
+</tr>
+
+`;
+
+
+});
+
+
+}
+
+
+
+loadProduct();
