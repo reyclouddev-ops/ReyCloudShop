217b82aaf0e07a53353cca0eaea832797e684522
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-15 18:19:10 +0700

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+
+const ProductSchema = new mongoose.Schema({
+
+name:{
+type:String,
+required:true
+},
+
+category:{
+type:String,
+required:true
+},
+
+description:String,
+
+price:Number,
+
+image:String,
+
+
+// khusus Roblox
+robloxUsername:String,
+
+robloxLink:String,
+
+
+status:{
+type:String,
+default:"active"
+}
+
+
+},{
+timestamps:true
+});
+
+
+module.exports =
+mongoose.model("Product",ProductSchema);
